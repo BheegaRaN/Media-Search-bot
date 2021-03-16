43b372c9cd721d739af1ccecacaa0cb8932262b2
BheeGu <66379969+BheegaRaN@users.noreply.github.com>
2021-03-16 18:32:21 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -60,7 +60,7 @@ def get_reply_markup(username):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
         InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-        InlineKeyboardButton('Share bot', url=url),
+        InlineKeyboardButton( "Join Channel", url="https://t.me/BM_Links", ),
     ]]
     return InlineKeyboardMarkup(buttons)
 
